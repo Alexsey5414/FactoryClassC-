8b7b56393e6297fab7de3e703295ff929f426302
Golubev <golubev_alexsey@mail.ru>
2025-12-10 12:10:33 +0300

    Добавил ограничение out

--- FactoryStorage/IFactoryTypeListStorage.cs
+++ FactoryStorage/IFactoryTypeListStorage.cs
@@ -2,7 +2,7 @@
 
 namespace ConsoleApp1.FactoryStorage
 {
-    public interface IFactoryTypeListStorage<T> where T : IBaseServiceFactory
+    public interface IFactoryTypeListStorage<out T> where T : IBaseServiceFactory
     {
         T GetFactory();
     }
