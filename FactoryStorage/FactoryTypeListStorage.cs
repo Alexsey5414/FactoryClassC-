c35ec518cc4200cdd9fa5b8a5448f5d74c4ca39b
Golubev <golubev_alexsey@mail.ru>
2025-12-10 12:05:38 +0300

    Добавил ограничение

--- FactoryStorage/FactoryTypeListStorage.cs
+++ FactoryStorage/FactoryTypeListStorage.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1.FactoryStorage
     public class FactoryTypeListStorage<T> : List<T>, IFactoryTypeListStorage<T> where T : IBaseServiceFactory
     {
         public T GetFactory() => this.Find(factory => factory is T);
-        public T GetFactory<Y>() => this.Find(factory => factory is Y);//factory.GetTypeFactory() == typeof(Y));
+        public T GetFactory<Y>() where Y: IBaseServiceFactory => this.Find(factory => factory is Y);//factory.GetTypeFactory() == typeof(Y));
         public FactoryTypeListStorage(IEnumerable<T> serviceFactories)
         {
             this.AddRange(serviceFactories);
